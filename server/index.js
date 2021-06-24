2bd99a8d31a6c2b4a16ee7551ef6b48d09d19139
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-24 12:46:25 -0500

    initial commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+app.get('/', function(req,res){
+    res.sendFile(path.join(__dirname, '../index.html'))
+})
+
+const port = process.env.PORT || 5050
+
+app.listen(port, function(){ console.log(`Server running on ${port}`)})
