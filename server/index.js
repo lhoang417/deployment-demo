80eb8096bcfbfd355bf521733fc5c3e07c716a96
Linh Hoang <Linh.Hoang@devmounta.in>
2021-06-24 13:21:43 -0500

    third push

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ const path = require('path')
 
 app.get('/', function(req,res){
     res.sendFile(path.join(__dirname, '../index.html'))
+    res.sendFile(path.join(__dirname, '../styles.css'))
 })
 
 const port = process.env.PORT || 5050
